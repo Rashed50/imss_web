00f1982867c94c2871844d8f94da05fcf163b55d
Rashed <rashed@Rasheds-MacBook-Pro.local>
2023-11-23 20:42:04 +0300

    update user date

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    // 
+    //
     /**
      * Run the migrations.
      *
@@ -17,8 +17,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('user_name');
+            $table->string('user_email')->unique();
+            $table->string('user_phone')->nullable();
+            $table->int('user_type')->default(1);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
@@ -26,11 +28,13 @@ class CreateUsersTable extends Migration
         });
 
         DB::table('users')->insert([
-            'name' => 'rashed' ,
+            'user_name' => 'rashed' ,
             'email' => 'admin@gmail.com' ,
+            'user_phone' => '01731540704',
+            'user_type' => 1,
             'password' => '$2y$10$oaXlh8Cj2YzlLqgXJCpeWeBQObF0ouFZcJBQKSgF4hCZhsqdrZL26' , // 123456789
         ]);
-        
+
     }
 
     /**
